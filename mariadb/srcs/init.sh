9f9428b97d450182d422b0387e6bd8ab1283c9a7
paulochon1202 <paul.boutelliez12@gmail.com>
2022-06-27 18:17:29 +0200

    pouletot

--- mariadb/srcs/init.sh
+++ mariadb/srcs/init.sh
@@ -4,5 +4,7 @@ rc-status
 touch /run/openrc/softlevel
 rc-service mariadb setup
 rc-service mariadb start
-mysql -u root -Bse "CREATE DATABASE wordpress;GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress password';FLUSH PRIVILEGES;"
+mysql -u root -Bse "CREATE DATABASE wordpress;GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'a';FLUSH PRIVILEGES;"
+rc-service mariadb stop
+sed -e "s/#bind/bind/" /etc/my.cnf.d/mariadb-server.cnf -i
 /usr/bin/mysqld_safe
