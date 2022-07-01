1cfeedefcd5ed8edef2460ba89e22a8bb79d98b8
nino <nino.faivre@netc.fr>
2022-07-01 22:32:22 +0200

    remove bugged wp core is-installed and add a pass for mariadb root user

--- srcs/mariadb/srcs/init.sh
+++ srcs/mariadb/srcs/init.sh
@@ -4,13 +4,16 @@ sed -e "s/#bind/bind/" /etc/my.cnf.d/mariadb-server.cnf -i
 sed -e "s/skip-networking/#skip-networking/" /etc/my.cnf.d/mariadb-server.cnf -i
 if [ ! -d /volumes/wordpressDB/mysql ]; then
 	mysql_install_db
+	/usr/share/mariadb/mysql.server start
+	mysql -u root -Bse "
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MariaDBRootPassword';"
+	/usr/share/mariadb/mysql.server stop
 fi
 /usr/share/mariadb/mysql.server start
-mysql -u root -Bse "
+mysql -u root -p"$MariaDBRootPassword" -Bse "
 CREATE DATABASE IF NOT EXISTS wordpress;
 GRANT ALL PRIVILEGES ON $WordpressDBName.* TO '$WordpressDBUser'@'%' IDENTIFIED BY '$WordpressDBPass';
 FLUSH PRIVILEGES;"
-#ALTER USER 'root'@'localhost' IDENTIFIED BY '$MariaDBRootPassword';
 /usr/share/mariadb/mysql.server stop
 
 exec "$@" # launch service
--- srcs/php/srcs/init.sh
+++ srcs/php/srcs/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-wp config create --path="/volumes/wordpressFiles" --skip-check --force --dbname="$WordpressDBName" --dbuser="$WordpressDBUser" --dbpass="$WordpressDBPass" --dbhost="mariadb"
-if wp core is-installed --path="/volumes/wordpressFiles"; then
-	wp core download --path="/volumes/wordpressFiles"
+if [ $(ls -l /volumes/wordpressFiles | tail -n +2 | wc -l) -eq "0" ]; then
+	wp core download
 fi
+wp config create --skip-check --force --dbname="$WordpressDBName" --dbuser="$WordpressDBUser" --dbpass="$WordpressDBPass" --dbhost="mariadb"
 
 sed "s/listen = 127.0.0.1:9000/listen = 9000/" /etc/php8/php-fpm.d/www.conf -i
 
