7ed56de1d903ae2fbc10a3de0f24dabc4e7cb8e1
nino <nino.faivre@netc.fr>
2022-07-05 17:59:11 +0200

    mariadb users host access and some simplifications

--- srcs/php/srcs/init.sh
+++ srcs/php/srcs/init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ $(ls -l /volumes/wordpressFiles | tail -n +2 | wc -l) -eq "0" ]; then
+if [ ! $(ls -A /volumes/wordpressFiles) ]; then
 	wp core download
 fi
 wp config create --skip-check --force --dbname="$WordpressDBName" --dbuser="$WordpressDBUser" --dbpass="$WordpressDBPass" --dbhost="mariadb"
 
-sed "s/listen = 127.0.0.1:9000/listen = 9000/" /etc/php8/php-fpm.d/www.conf -i
+sed "s/listen = 127.0.0.1:9000/listen = php:9000/" /etc/php8/php-fpm.d/www.conf -i
 
 exec "$@" # launch service
