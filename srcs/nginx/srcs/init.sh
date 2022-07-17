60429e9c8a8eca1734c9eaf5a485081029e28c67
Nino Faivre <60893869+ninofaivre@users.noreply.github.com>
2022-07-17 17:10:29 +0200

    remove init.sh of nginx tsl key gen (it's already in the dockerfile)

--- srcs/nginx/srcs/init.sh
+++ srcs/nginx/srcs/init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 chown nginx:nginx /volumes/wordpressFiles
-openssl genrsa -out test.key 2048
-openssl req -x509 -nodes -days 365 -key test.key -out test.crt -subj "/C=FR/ST=France/L=Paris/O=42/OU=42Paris/CN=42/emailAddress=42@42.42"
 
 exec "$@" # launch service
